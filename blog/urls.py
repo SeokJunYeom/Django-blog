272e3d856f6ede842b083a6dd4938551bd5283c4
SeokJunYeom <tjrwnsdl0720@naver.com>
2016-08-31 23:17:30 +0900

    materialize

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.gochiusa, name = 'gochiusa'),
         url(r'(?P<name>\w+)/$', views.character, name = 'character'),
+        url(r'^test$', views.test, name = 'test'),
 ]
