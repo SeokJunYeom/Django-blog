418a07380406927d1184ccd1277fbd4f6f82bc9f
SeokJunYeom <tjrwnsdl0720@naver.com>
2016-09-06 09:56:53 +0900

    custom 404 error page

--- unifox/urls.py
+++ unifox/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import include, url, handler404
+from django.conf.urls import include, url
 from django.contrib import admin
+import handler.views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^gochiusa/', include('blog.urls')),
+    url(r'', handler.views.custom_404, name = '404'),
 ]
-
-handler404 = 'handler.views.custom_404'
