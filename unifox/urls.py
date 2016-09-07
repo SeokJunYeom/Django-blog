43c40d746b7d5a8306550847f86d58bdbdfc0ca1
SeokJunYeom <tjrwnsdl0720@naver.com>
2016-09-07 19:45:20 +0900

    favicon

--- unifox/urls.py
+++ unifox/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 import handler.views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^gochiusa/', include('blog.urls')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url = '/static/favicon.ico')),
     url(r'', handler.views.custom_404, name = '404'),
 ]
