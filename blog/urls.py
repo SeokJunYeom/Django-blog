d16a8e975090bbd44bfe1874b4eecee0470aa665
SeokJunYeom <tjrwnsdl0720@naver.com>
2016-09-25 22:17:12 +0900

    login page

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login
 from . import views
 
 urlpatterns = [
 	url(r'^$', views.gochiusa, name = 'gochiusa'),
 	url(r'post/$', views.character_post, name = 'character_post'),
+	url(r'login/$', login, name = 'login', kwargs = {'template_name' : 'login.html'}),
 	url(r'(?P<name>\w+)/$', views.character, name = 'character'),
 ]
