b817f6a4e93e5e56b8da324263d0ddb5d13b479c
SeokJunYeom <tjrwnsdl0720@naver.com>
2016-10-05 23:53:15 +0900

    logout

--- gochiusa/urls.py
+++ gochiusa/urls.py
@@ -3,13 +3,15 @@ from django.conf.urls import url
 from django.views.generic import RedirectView
 from django.views.static import serve
 from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
 from . import views
 
 urlpatterns = [
 	url(r'^$', views.gochiusa, name = 'gochiusa'),
         url(r'^favicon\.ico$', RedirectView.as_view(url = '/static/favicon.ico')),
 	url(r'post/$', views.character_post, name = 'character_post'),
-	url(r'login/$', login, name = 'login', kwargs = {'template_name' : 'login.html'}),
+	url(r'accounts/login/$', login, name = 'login', kwargs = {'template_name' : 'login.html'}),
+        url(r'accounts/logout/$', logout, name = 'logout'),
 	url(r'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 	url(r'(?P<name>\w+)/$', views.character, name = 'character'),
 ]
